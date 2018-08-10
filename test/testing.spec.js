3b7157f550cd1d5ec87054e53f68c4c52bb0750c
valeria <valeria.penaloza5@gmail.com>
2018-08-10 12:05:58 -0400

    estructura basica y deploy

---
+++ test/testing.spec.js
@@ -0,0 +1,6 @@
+onst assert = require('chai').assert;
+global.window = global;
+//enlaza el archivo
+require('../src/js/view.js');
+require('../src/js/model.js');
+require('../src/js/controller.js');
