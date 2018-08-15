42bb8eab5b819add4e782f63ea9e2ea8312470e7
valeria <valeria.penaloza5@gmail.com>
2018-08-14 22:27:19 -0300

    maquetacion, mostrando imagenes y mostrando modal

--- docs/src/js/model.js
+++ docs/src/js/model.js
@@ -1,1 +1,16 @@
 window.model = {};
+//llamando a la api de imagenes randomf
+window.onload = () => {
+	fetch('src/js/photos.json')
+	.then(res => res.json())
+	.then(photoJSON => {
+		// console.log(photoJSON);
+		window.view.viewPhoto(photoJSON);
+	});
+};
+
+
+
+
+
+
--- src/js/model.js
+++ src/js/model.js
@@ -1,21 +1,16 @@
 window.model = {};
 //llamando a la api de imagenes randomf
-window.model.callApi = () => {
-	fetch('https://cors-anywhere.herokuapp.com/http://shibe.online/api/cats?count=10&urls=true&httpsUrls=true')
+window.onload = () => {
+	fetch('src/js/photos.json')
 	.then(res => res.json())
-	.then(dogJSON => {
-		dogPhoto = dogJSON;
-		console.log(dogPhoto);
+	.then(photoJSON => {
+		// console.log(photoJSON);
+		window.view.viewPhoto(photoJSON);
 	});
-	// let photo = document.getElementById('photo');
-	const dogApi = (dogPhoto) =>{
-		for (let i in dogPhoto){
-			let dogi = dogPhoto[i].message;
-		}
-		console.log(dogi);
-		return dogPhoto;
-	}
 };
+
+
+
 
 
 
